5e4d391ee41ccc03bfd5e4bb35ae21c518fdf8ff
ITR <ITR@live.no>
2020-07-15 05:54:07 +0200

    Updated Version

--- MelonCameraMod/Properties/AssemblyInfo.cs
+++ MelonCameraMod/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using MelonCameraMod;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: MelonModInfo(typeof(CameraMod), "ITR's Melon Cameras", "1.2.1", "ITR")]
+[assembly: MelonModInfo(typeof(CameraMod), "ITR's Melon Cameras", "1.3.0", "ITR")]
 [assembly: MelonModGame(null, null)]
