5eb019fec0470c19e03157feaa2ba43fc83dcd92
ITR <ITR@live.no>
2020-07-18 15:18:33 +0200

    Made repo work with legacy input modules

---
+++ MelonCameraMod-LegacyInput/CameraMod.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
+
+namespace MelonCameraMod
+{
+    public class CameraMod : MelonMod
+    {
+        private class CameraData
+        {
+            public Camera Camera;
+            public KeyCode Hold, Press;
+        }
+
+        private GameObject _cameraParent;
+        private readonly List<CameraData> _cameras = new List<CameraData>();
+
+        public override void OnApplicationQuit()
+        {
+            ConfigWatcher.Unload();
+        }
+
+        public override void OnLevelWasInitialized(int level)
+        {
+            _cameraParent = null;
+        }
+
+        public override void OnUpdate()
+        {
+            CheckToggles();
+
+            if (!ConfigWatcher.UpdateIfDirty() && _cameraParent != null) return;
+
+            var mainCamera = Camera.main;
+            if (mainCamera == null) return;
+            _cameraParent = mainCamera.gameObject;
+
+            UpdateCameras();
+        }
+
+        private void CheckToggles()
+        {
+            foreach (var cameraData in _cameras)
+            {
+                if (cameraData.Press == KeyCode.None) continue;
+                if (cameraData.Hold != KeyCode.None && !Input.GetKey(cameraData.Hold)) continue;
+                if (!Input.GetKeyDown(cameraData.Press)) continue;
+                if (cameraData.Camera == null) continue;
+                cameraData.Camera.enabled ^= true;
+            }
+        }
+
+        private void UpdateCameras()
+        {
+            foreach (var cameraData in _cameras)
+            {
+                if (cameraData.Camera == null) continue;
+                Object.Destroy(cameraData.Camera.gameObject);
+            }
+            _cameras.Clear();
+
+            var cameraCount = ConfigWatcher.CameraConfigs?.Count ?? 0;
+            MelonModLogger.Log($"Creating {cameraCount} cameras");
+
+            for (var i = 0; i < cameraCount; i++)
+            {
+                // ReSharper disable once PossibleNullReferenceException
+                var config = ConfigWatcher.CameraConfigs[i];
+                if (config == null)
+                {
+                    MelonModLogger.LogWarning($"Camera {i} in config is null");
+                    continue;
+                }
+
+                var child = new GameObject($"Modded Camera {i}");
+                var parent = _cameraParent.transform;
+                if (!string.IsNullOrWhiteSpace(config.ParentGameObject))
+                {
+                    var newParent = GameObject.Find(config.ParentGameObject);
+                    if (newParent == null)
+                    {
+                        MelonModLogger.Log($"Failed to find gameobject '{config.ParentGameObject}'");
+                    }
+                    else
+                    {
+                        parent = newParent.transform;
+                    }
+                }
+
+                for (var j = 0; j < config.ParentAscension; j++)
+                {
+                    if (parent.parent == null)
+                    {
+                        MelonModLogger.Log($"Failed to ascend parent {j} times (goal was {config.ParentAscension})");
+                        break;
+                    }
+
+                    parent = parent.parent;
+                }
+
+                child.transform.parent = parent;
+
+
+                var camera = child.AddComponent<Camera>();
+                _cameras.Add(
+                    new CameraData
+                    {
+                        Camera = camera,
+                        Hold = config.HoldToToggle,
+                        Press = config.PressToToggle,
+                    }
+                );
+
+                camera.enabled = config.Enabled;
+                child.transform.localPosition = config.LocalPosition;
+
+                if (config.UseRotation)
+                {
+                    child.transform.localRotation = config.LocalRotation;
+                }
+                else
+                {
+                    child.transform.LookAt(
+                        _cameraParent.transform,
+                        _cameraParent.transform.up
+                    );
+                }
+
+                if (config.UseAspect)
+                {
+                    camera.aspect = config.Aspect;
+                }
+
+                camera.backgroundColor = config.BackgroundColor;
+                camera.clearFlags = config.ClearFlags;
+                camera.depth = config.Depth;
+                camera.farClipPlane = config.FarClipPlane;
+                camera.nearClipPlane = config.NearClipPlane;
+                camera.orthographic = config.Orthographic;
+                camera.orthographicSize = config.OrthographicSize;
+                camera.rect = config.Rect;
+                camera.cullingMask = (int)config.CullingMask;
+
+                camera.eventMask = 0;
+                camera.stereoTargetEye = StereoTargetEyeMask.None;
+            }
+        }
+    }
+}
---
+++ MelonCameraMod-LegacyInput/ConfigWatcher.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MelonLoader;
+using MelonLoader.TinyJSON;
+using UnityEngine;
+
+namespace MelonCameraMod
+{
+    static class ConfigWatcher
+    {
+        private const string FileName = "CameraConfig.json";
+
+        private static readonly string FileDirectory = Path.Combine(
+            Environment.CurrentDirectory,
+            "UserData"
+        );
+
+        private static readonly string FullPath = Path.Combine(
+            FileDirectory,
+            FileName
+        );
+
+        public static List<CameraConfig> CameraConfigs =
+            new List<CameraConfig>();
+
+        private static readonly FileSystemWatcher FileSystemWatcher;
+        private static bool _dirty = false;
+
+        static ConfigWatcher()
+        {
+            FileSystemWatcher = new FileSystemWatcher(FileDirectory, FileName)
+            {
+                NotifyFilter = (NotifyFilters)((1 << 9) - 1),
+                EnableRaisingEvents = true
+            };
+            FileSystemWatcher.Changed += (_, __) => _dirty = true;
+            FileSystemWatcher.Created += (_, __) => _dirty = true;
+            FileSystemWatcher.Renamed += (_, __) => _dirty = true;
+            FileSystemWatcher.Deleted += (_, __) => _dirty = true;
+            _dirty = true;
+        }
+
+        public static void Unload()
+        {
+            FileSystemWatcher.EnableRaisingEvents = false;
+            _dirty = false;
+        }
+
+        public static bool UpdateIfDirty()
+        {
+            if (!_dirty) return false;
+            _dirty = false;
+
+            if (!File.Exists(FullPath))
+            {
+                MelonModLogger.Log(
+                    $"Creating default config file at \"{FullPath}\""
+                );
+                var sampleConfig = new List<CameraConfig>
+                {
+                    new CameraConfig(),
+                    new CameraConfig
+                    {
+                        Aspect = 1,
+                        Rect = new SerializedRect(0, 0, 0.25f, 0.25f),
+                        UseAspect = true,
+                        UseRotation = true,
+                    }
+                };
+
+                var json = JSON.Dump(
+                    sampleConfig,
+                    EncodeOptions.PrettyPrint | EncodeOptions.NoTypeHints
+                );
+                File.WriteAllText(FullPath, json);
+            }
+
+            MelonModLogger.Log("Updating camera configs");
+
+            CameraConfigs.Clear();
+
+            try
+            {
+                var json = File.ReadAllText(FullPath);
+                JSON.MakeInto(JSON.Load(json), out CameraConfigs);
+            }
+            catch (Exception e)
+            {
+                MelonModLogger.LogError(e.ToString());
+                MelonModLogger.Log(
+                    "Something went wrong when deserializing json. Check the ReadMe in case something has changed"
+                );
+            }
+
+            CameraConfigs = CameraConfigs ?? new List<CameraConfig>();
+
+            return true;
+        }
+    }
+}
