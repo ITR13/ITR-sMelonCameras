82e15f5ba9946d58aac8a728a384ce9c67fa12a0
ITR <ITR@live.no>
2020-08-13 23:21:30 +0200

    Added assembly info to legacy input mod

--- MelonCameraMod-LegacyInput/Properties/AssemblyInfo.cs
+++ MelonCameraMod-LegacyInput/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using MelonCameraMod;
+using MelonLoader;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +36,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonModInfo(typeof(CameraMod), "ITR's Melon Cameras", "1.3.0", "ITR")]
+[assembly: MelonModGame(null, null)]
